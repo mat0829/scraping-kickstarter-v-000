16e0d56fada509261829126e30a8d1b9ebad75f8
Mat Anderson <matandersonlv@hotmail.com>
2019-03-18 21:25:16 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 require 'nokogiri'
 
 def create_project_hash
-  html = File.read("fixtures/kickstarter.html")
+  html = File.read('fixtures/kickstarter.html')
  
   kickstarter = Nokogiri::HTML(html)
 end
