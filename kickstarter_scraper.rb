0e2f943dec548da14a53571af01b6c5c7ecda158
Mat Anderson <matandersonlv@hotmail.com>
2019-03-18 21:26:06 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,8 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 end
 
+create_project_hash
+
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 
